e76e28be09b16e2eb7576932de740fb03c14678f
cortes150 <chicholas150@gmail.com>
2024-09-24 21:04:35 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: 'AIzaSyDcfb_8zkPovC-msWhu7zh7u7xWw313x7Q',
-    authDomain: 'proyecto-unandes-mani.firebaseapp.com',
-    databaseURL: 'https://proyecto-unandes-mani-default-rtdb.firebaseio.com',
-    projectId: 'proyecto-unandes-mani',
-    storageBucket: 'proyecto-unandes-mani.appspot.com',
-    messagingSenderId: '358533483820',
-    appId: '1:358533483820:web:6c7088499a976b2a56b089',
-  },
+
 };
